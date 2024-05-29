676bbd549a0f0654631af385d01f303a617b471a
HYUNHO <jeny95kr@gmail.com>
2024-05-29 01:25:07 -0400

    fixed API 405 errro

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": "https://cleanuri.com",
-    },
-  },
   base: "/url-shortening-page/",
 });

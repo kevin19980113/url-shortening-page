104c3902c4eca2c2e595218552aebcd20885acd2
HYUNHO <jeny95kr@gmail.com>
2024-05-29 02:25:08 -0400

    fixed API 405 errror

--- src/dataFetch.js
+++ src/dataFetch.js
@@ -6,6 +6,7 @@ export async function getShortenUrl(url) {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
     },
     body: JSON.stringify({
       url: url,
